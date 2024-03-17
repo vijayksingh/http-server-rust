418188578b3c7e24a91e7ee9e6aea5674f2af32f
Vijay Singh <dprophecyguy@gmail.com>
2024-03-17 16:46:53 +0530

    Added the ability to read path and respond with 200 or 404 based on path comparison

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,21 @@ fn main() {
         match stream {
             Ok(mut _stream) => {
                 let _bytes_read = _stream.read(&mut buffer).unwrap();
-                let response = format!("HTTP/1.1 200 OK\r\n\r\n");
+                let data = std::str::from_utf8(&buffer[.._bytes_read]).expect("Invalid UTF-8");
+                let path_line: Vec<&str> = data
+                    .lines()
+                    .rev()
+                    .last()
+                    .unwrap()
+                    .split_whitespace()
+                    .collect();
+
+                let response: String;
+                if path_line[1] == "/" {
+                    response = format!("HTTP/1.1 200 OK\r\n\r\n");
+                } else {
+                    response = format!("HTTP/1.1 404 NOT FOUND\r\n\r\n");
+                }
 
                 _stream.write(response.as_bytes()).unwrap();
             }
